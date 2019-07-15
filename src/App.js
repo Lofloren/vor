d3a0f967bf09cfc7629c7f42a8cd9f4418c7aed7
Lofloren <smoochym@hotmail.co.uk>
2019-07-15 17:31:13 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Particles from 'react-particles-js';
+import posed from 'react-pose';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 
 
 function Index() {
-  return <h2>Home</h2>;
+
+return ( <div style={{color: 'white', textAlign : 'center', padding: '5%'}}>
+  Coming soon...
+  </div>)
+  
+  
 }
 
 function Privacy() {
@@ -18,7 +24,7 @@ function Privacy() {
   <p>I will only ask for personal information when I truly need it to provide a service to you. I collect it by fair and lawful means, with your knowledge and consent. I will also let you know why I'm collecting it and how it will be used.</p>
   <p>I only retain collected information for as long as necessary to provide you with your requested service. What data I store, I’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorised access, disclosure, copying, use or modification.</p>
   <p>I don’t share any personally identifying information publicly or with any third-parties, except when required to by law.</p>
-  <p>Our games link to external services that are not operated by us. Please be aware that I have no control over the content and practices of these services, and cannot accept responsibility or liability for their respective privacy policies.</p>
+  <p>My games link to external services that are not operated by me. Please be aware that I have no control over the content and practices of these services, and cannot accept responsibility or liability for their respective privacy policies.</p>
   <p> </p>
   <p style={{textDecoration: 'underline'}}> Unity Ads</p>
   <p/>
@@ -28,23 +34,25 @@ function Privacy() {
   <p/>
   <p style={{textDecoration: 'underline'}}> Unity Analytics</p>
   <p/>
-  <p>This application collects general usage data using Third-party Analytics services. For example, this allows us to know things like the amount of users we have and what types of devices they use. The privacy policies of the Analytics services used are linked below.</p>
+  <p>This application collects general usage data using Third-party Analytics services. For example, this allows me to know things like the amount of players I have and what types of hardware they use. The privacy policies of the Analytics services used are linked below.</p>
   <p/>
   <p><a href='https://unity3d.com/legal/privacy-policy' style={{textDecoration: 'none'}}> Unity Privacy Policy </a> </p>
   <p/>
-  <p>You are free to refuse our request for your personal information, or to examine or have destroyed any information collected on you, with the understanding that I may be unable to provide you with some of your desired services.</p>
-  <p>Your continued use of our games will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact me at: </p>
+  <p>You are free to refuse my request for your personal information, or to examine or have destroyed any information collected on you, with the understanding that I may be unable to provide you with some of your desired services.</p>
+  <p>Your continued use of my games will be regarded as acceptance of my practices around privacy and personal information. If you have any questions about how I handle user data and personal information, feel free to contact me, Alexander Motch, at: </p>
   <p> alexandermotch@gmail.com </p>
 
   <p>This policy is effective as of 14 July 2019.</p>
   <p/>
-  
+
+
   </div>);
 }
 
 
 
 function App() {
+
   return (
    
     <Router>
@@ -135,11 +143,12 @@ function App() {
                 }}
                 style={{
                         width: '100%',
-                        background: `#000000` 
+                       
                  }}
                 >
 
-                  </Particles>
+                  </Particles> 
+               
         <nav>
                  <div  className="spacing">
               <Link to="/" className="MenuLink">Home</Link>
